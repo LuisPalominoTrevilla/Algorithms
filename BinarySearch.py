ce2e1799e5654e9e079125e53b694ad5bbe0339f
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-09-12 20:21:31 -0500

    Added Binary Search Iterative

--- BinarySearch.py
+++ BinarySearch.py
@@ -9,9 +9,7 @@ Created on Thu Sep  6 10:29:12 2018
 
 def binarySearch(x, n):
     if len(x) == 1:
-        if x[0] == n:
-            return True
-        return False
+        return x[0] == n
     mid = len(x)//2
     if x[mid] == n:
         return True
@@ -20,5 +18,21 @@ def binarySearch(x, n):
     else:
         return binarySearch(x[0:mid], n)
 
+def bSearchIterative(x, num):
+    low = 0
+    high = len(x)-1
+    
+    while(True):
+        mid = (low+high)//2
+        if high < low:
+            return False
+        elif x[mid] == num:
+            return (True, mid)
+        elif x[mid] > num:
+            high = mid-1
+        else:
+            low = mid+1
+            
+
 lista = [1,2,3,4,5,6,7,8,9]
-print(binarySearch(lista, 9))
+print(bSearchIterative(lista, 1))
