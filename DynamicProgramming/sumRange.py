0a97bdc37d9a33e9e249d9b284251c7df2c2ac68
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-10-18 10:38:10 -0500

    Simplified sumRange

--- DynamicProgramming/sumRange.py
+++ DynamicProgramming/sumRange.py
@@ -1,12 +1,8 @@
 def prep(arrayA):
-    sums = [[arrayA[j] if j == i else 0 for j in range(len(arrayA))] for i in range(len(arrayA))]
-    for j in range(len(arrayA)):
-        for i in range(len(arrayA)-1,-1,-1):
-            if i < j:
-                sums[i][j] = sums[i][i]+sums[i+1][j]
-    return sums
+    for i in range(1, len(arrayA)):
+        arrayA[i] = arrayA[i]+arrayA[i-1]
 def sumRange(start, end, sums):
-    return sums[start][end]
+    return sums[end] if start == 0 else sums[end]-sums[start-1]
 arrayA = [1,-2,3,10,-8,0]
-sums = prep(arrayA)
-print(sumRange(3,3, sums))
+prep(arrayA)
+print(sumRange(1,3, arrayA))
