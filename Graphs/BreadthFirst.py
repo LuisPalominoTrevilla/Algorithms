e30e693e0c2317986bf6041bc356abffdb114e94
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-11-07 17:55:31 -0600

    Added BFS algorithm

---
+++ Graphs/BreadthFirst.py
@@ -0,0 +1,47 @@
+from collections import deque
+class Vertice:
+    def __init__(self, val):
+        self.history = []
+        self.value = val
+        self.adjacent = []
+    def addAdjacent(self, adj):
+        self.adjacent.append(adj)
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    def addEdge(self, u, v):
+        if u not in self.vertices:
+            self.vertices[u] = Vertice(u)
+        if v not in self.vertices:
+            self.vertices[v] = Vertice(v)
+        self.vertices[u].addAdjacent(self.vertices[v])
+        self.vertices[v].addAdjacent(self.vertices[u])
+def BreadthWiseSearch(g, start, end):
+    work = deque()
+    visited = set()
+    start.history = []
+    work.appendleft(start)
+    visited.add(start)
+    while len(work):
+        current = work.pop()
+        if (current == end):
+            current.history.append(current)
+            return current.history.copy()
+        else:
+            neighbors = current.adjacent
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    neighbor.history = current.history.copy()
+                    neighbor.history.append(current)
+                    work.appendleft(neighbor)
+                    visited.add(neighbor)
+    return None
+        
+graphRep = open("./simpleGraph.txt", "r")
+graph = Graph()
+for line in graphRep:
+    result = [int(x) for x in line.split()]
+    graph.addEdge(result[0], result[1])
+print([x.value for x in BreadthWiseSearch(graph, graph.vertices[9], graph.vertices[2])])
+print([x.value for x in BreadthWiseSearch(graph, graph.vertices[2], graph.vertices[7])])
+print([x.value for x in BreadthWiseSearch(graph, graph.vertices[1], graph.vertices[10])])
